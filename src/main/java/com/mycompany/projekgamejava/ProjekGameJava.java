72bb8b747191e7c8fbeb0b370ff00acb9e4f7fe5
Zaid Akmal Firdaus <zangirrhehe@gmail.com>
2026-04-06 11:53:11 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/projekgamejava/ProjekGameJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projekgamejava;
+
+/**
+ *
+ * @author Zaid Akmal
+ */
+public class ProjekGameJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
